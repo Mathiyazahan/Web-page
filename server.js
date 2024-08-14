44ef3b454d1a5ec41877ea2dfb57b2928e244649
Mathiyazahan <73736289+Mathiyazahan@users.noreply.github.com>
2024-08-14 15:14:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.get('/pod-ip', (req, res) => {
     res.json({ podIp });
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
