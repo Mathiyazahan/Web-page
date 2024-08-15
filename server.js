d61bf93fcd6887099f474b895ed3a83cdea258af
Mathiyazahan <73736289+Mathiyazahan@users.noreply.github.com>
2024-08-15 10:53:10 +0530

    Update and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const os = require('os');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('.'));
+
+// Endpoint to get pod information
+app.get('/info', (req, res) => {
+    const podIP = req.ip || 'unknown';
+    console.log(`Pod IP Address: ${podIP}`); // Log the IP address to the console
+    res.json({
+        hostname: os.hostname(),
+        podId: process.env.POD_ID || 'unknown',
+        podIP: podIP
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`);
+});
