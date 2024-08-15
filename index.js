e6f65e13e4768bc0843324a6e586ba04230b2ed9
Mathiyazahan <73736289+Mathiyazahan@users.noreply.github.com>
2024-08-15 11:42:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const express = require('express');
-const os = require('os');
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  const podHostname = os.hostname();
-  const podIpAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.send(`
-    <html>
-    <head><title>Pod Information</title></head>
-    <body>
-      <h1>Pod Information</h1>
-      <p>Pod Hostname: ${podHostname}</p>
-      <p>Pod IP Address: ${podIpAddress}</p>
-    </body>
-    </html>
-  `);
+    const podIp = process.env.POD_IP || 'unknown';
+    const hostname = require('os').hostname();
+    res.send(`<h1>Pod Information</h1>
+              <p>Pod Hostname: ${hostname}</p>
+              <p>Pod IP Address: ${podIp}</p>`);
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://0.0.0.0:${port}/`);
+    console.log(`Server running on port ${port}`);
 });
